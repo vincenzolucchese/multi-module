e3375cd11db98c6a79dc61f3e7d3bb96825b441b
Vincenzo Lucchese <vlucchese@altevie.com>
2020-04-09 23:48:01 +0200

    simmetric encryption by command line

--- client-ws-ssh/src/main/java/com/example/springboot/Application.java
+++ client-ws-ssh/src/main/java/com/example/springboot/Application.java
@@ -4,10 +4,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.security.PublicKey;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
+import java.util.Base64;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.net.ssl.SSLContext;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
@@ -75,8 +81,45 @@ public class Application {
 		FileInputStream inputStream  =  new FileInputStream (resource.getFile());
 		CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
 		X509Certificate cert = (X509Certificate)certFactory.generateCertificate(inputStream);
-		
-		cert.getPublicKey().getAlgorithm().getEncoded();
+		System.out.println(cert.toString());
+
+		KeyGenerator keygen = KeyGenerator.getInstance("AES");
+	    SecretKey aesKey = keygen.generateKey();
+	    
+	    Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+//	    Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+	    // Initialize the cipher for encryption
+	    aesCipher.init(Cipher.ENCRYPT_MODE, aesKey);
+
+	    // Our cleartext
+	    byte[] cleartext = "This is just an example".getBytes();
+	    System.out.println(new String(cleartext));
+	    
+	    byte[] pincode = "1426073406".getBytes();
+	    byte[] pincodeCry = aesCipher.doFinal(pincode);
+	    System.out.println(Arrays.toString(pincodeCry));
+
+	    
+	    // Encrypt the cleartext
+	    byte[] ciphertext = aesCipher.doFinal(cleartext);
+
+	    // Initialize the same cipher for decryption
+	    aesCipher.init(Cipher.DECRYPT_MODE, aesKey);
+
+	    // Decrypt the ciphertext
+	    byte[] cleartext1 = aesCipher.doFinal(ciphertext);
+	    
+	    System.out.println(new String(cleartext1));
+	    
+	    String example = "txIO+4Z3DqZvxrol9o6KmkQjlWSD4SOLdeUigRadhoxVNv7rM3RytJlT1jCHdMEg337cD3D1bvYd/uFLeBT+uZzzZy67Rx5UlWD6bvqXlRQjAMxDH/oGcsxmLtBqLPe8i/ZHdiE1xgEPWj+OX6bOdZJOaQgCa9LY8WQaWOWSvbA=";
+	    
+	    byte[] base64decodedTokenArr = Base64.getDecoder().decode(example);
+	    byte[] decryptedPassword = aesCipher.doFinal(base64decodedTokenArr);
+	    
+	    System.out.println(new String(decryptedPassword));
+	    
+	    
 	}
 	
 	
